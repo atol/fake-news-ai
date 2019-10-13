079e47e7d6c6336e5efe65baddbdbbd08f253fd2
blvnder <alicelinlc@gmail.com>
2019-10-12 21:20:06 -0700

    Add F1 score to score.py

--- score.py
+++ score.py
@@ -1,10 +1,35 @@
-# Simple scoring function
+import json
+from classifiers import *
+from sklearn.metrics import f1_score
+
+METADATA_FILEPATH = 'dataset/metadata.json'
+ARTICLES_FILEPATH = 'dataset/articles'
+
+# F1 score for predictions.txt
+def f1(claims):
+    true = [cl['label'] for cl in claims]
+    
+    pred = []
+    f = open('answer/predictions.txt', 'r')
+    for l in f:
+        line = l.strip()
+        pred.append(int(line[-1:]))
+
+    result = f1_score(true, pred, average='macro')
+    return result
+
+# F1 score for individual classifiers
+def f1_clf(claims, classifier):
+    true = []
+    pred = []
+    for cl in claims:
+        true.append(cl['label'])
+        pred.append(classifier(cl))
+    result = f1_score(true, pred, average='macro')
+    return result
 
 if __name__ == '__main__':
-    num_claims = 15555
+    with open(METADATA_FILEPATH, 'r') as f:
+        claims = json.load(f)
 
-    with open ('predictions.txt', 'r') as predictions:
-        with open('solutions.txt', 'r') as solutions:
-            same = set(predictions).intersection(solutions)
-    
-    print("score: {:.5f}".format(len(same)/num_claims))
+    print("dev score:", f1(claims))
