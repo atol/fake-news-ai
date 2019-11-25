cc6a3006366b275b4f01fc260a218a8837ed384c
blvnder <alicelinlc@gmail.com>
2019-11-24 22:56:04 -0800

    Combined claim and claimant model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
-from classifiers import *
+import pickle
+import pandas as pd
 
 # These are the file paths where the validation/test set will be mounted (read only)
 # into your Docker container.
@@ -11,17 +12,31 @@ ARTICLES_FILEPATH = 'dataset/articles'
 PREDICTIONS_FILEPATH = 'predictions.txt'
 
 if __name__ == '__main__':
-    # Set up classifiers and weights
-    classifiers = get_classifiers()
-    weights = get_weights(classifiers, eval_mcc)
+    # Load learned model
+    with open('models/combined_claim_claimant.pkl', 'rb') as f:
+        model = pickle.load(f)
 
-    # Read in the metadata file.
+    # Read in the metadata file
     with open(METADATA_FILEPATH, 'r') as f:
-        claims = json.load(f)
+        data = json.load(f)
     
-    # Create a predictions file.
+    # Convert metadata file to dataframe
+    df = pd.DataFrame(data)
+
+    # Combine claim and claimant
+    df["combined"] = df["claim"].map(str) + ' ' + df["claimant"]
+    X = df.combined
+    y = df.label
+
+    # Use model to make predictions
+    pred = model.predict(X)
+
+    # Join predictions to dataframe
+    df['prediction'] = pred
+
+    # Create a predictions file
     print('\nWriting predictions to:', PREDICTIONS_FILEPATH)
     with open(PREDICTIONS_FILEPATH, 'w') as f:
-        for claim in claims:
-            f.write('%d,%d\n' % (claim['id'], weighted_voting_classifier(claim, classifiers, weights)))
+        for index, row in df.iterrows():
+            f.write('%d,%d\n' % (row['id'], row['prediction']))
     print('Finished writing predictions.')
